79f37501ec15ffdcfd9de361eb93c4074c2efae1
MatteoPierro <pierromatteo@gmail.com>
2017-02-03 14:10:28 +0100

    unifies tests

--- java/src/test/java/com/socialnetwork/SocialNetworkShould.java
+++ java/src/test/java/com/socialnetwork/SocialNetworkShould.java
@@ -13,30 +13,15 @@ public class SocialNetworkShould {
     @Mock private SocialNetwork socialNetwork;
 
     @Test
-    public void publish_message_to_alice_time_line() {
+    public void publish_messages_to_user_timeline() throws Exception {
         SocialNetworkCommandLine commandLine = new SocialNetworkCommandLine(socialNetwork);
 
         commandLine.execute("Alice -> I love the weather today");
-
-        verify(socialNetwork).publish("Alice", "I love the weather today");
-    }
-
-    @Test
-    public void publish_message_to_bob_time_line() {
-        SocialNetworkCommandLine commandLine = new SocialNetworkCommandLine(socialNetwork);
-
         commandLine.execute("Bob -> Damn! We lost!");
-
-        verify(socialNetwork).publish("Bob", "Damn! We lost!");
-    }
-
-    @Test
-    public void publish_another_message_to_bob_time_line() {
-        SocialNetworkCommandLine commandLine = new SocialNetworkCommandLine(socialNetwork);
-
         commandLine.execute("Bob -> Good game though.");
 
+        verify(socialNetwork).publish("Alice", "I love the weather today");
+        verify(socialNetwork).publish("Bob", "Damn! We lost!");
         verify(socialNetwork).publish("Bob", "Good game though.");
     }
-
 }
